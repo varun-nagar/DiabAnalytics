2e5737292d18b9ad1d5d27762561a2eff831a592
Varunsingh Nagar <varunsingh.nagar@synapse.com>
2017-02-17 15:56:54 +0530

    initiation to add dynamic background

--- public/client/controllers/mainCtrl.js
+++ public/client/controllers/mainCtrl.js
@@ -4,4 +4,13 @@ angular.module("appModule").controller("mainCtrl", ["$scope", "$rootScope", "che
     $scope.openSideBar = function () {
         document.getElementById("sideNavBar").style.width = "250px";
     };
+
+    $rootScope.$watch(function (scope) { return scope.isLogin; }, function (newVal, oldVal) {
+        if (newVal) {
+            document.getElementsByTagName("body")[0].style.background="none";
+        } else {
+            document.getElementsByTagName("body")[0].style.background="-webkit-radial-gradient(rgba(255,255,255,0.5),rgba(255,255,255,0.5)), url('https://assets.bwbx.io/images/users/iqjWHBFdfxIU/iLwW0x7XKLHM/v3/-1x-1.jpg')";            
+        }
+    })
+
 }]);
