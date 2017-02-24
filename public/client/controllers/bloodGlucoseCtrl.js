27ea35cd7a6925af77c776123b45486631e6b6eb
varun nagar <varunnagar89@gmail.com>
2017-02-25 00:02:17 +0530

    add multiple graph tiles.

--- public/client/controllers/bloodGlucoseCtrl.js
+++ public/client/controllers/bloodGlucoseCtrl.js
@@ -1,18 +1,31 @@
-angular.module("appModule").controller("bloodGlucoseCtrl", ["$scope","$timeout", function ($scope,$timeout) {
-    $scope.graphTiles = [
-        {
-            key: "svg1",
-            title: "before breakfast",
-            width: "350",
-            height: "350"
-        },
-        {
-            key: "svg2",
-            title: "after breakfast",
-            width: "350",
-            height: "350"
-        }
-    ];
+angular.module("appModule").controller("bloodGlucoseCtrl", ["$scope", "$timeout", function ($scope, $timeout) {
+
+    const cardWidth = 350;
+    const cardHeight = 350;
+
+    var creatGraphTiles = function () {
+        var graphTilesArr = [];
+        var meals = ["breakfast", "lunch", "dinner"]
+        var keyIndex = 0;
+        
+        meals.forEach(function (meal) {
+            graphTilesArr.push({
+                key: "svg" + keyIndex++,
+                title: "before " + meal,
+                width: cardWidth,
+                height: cardHeight
+            });
+            graphTilesArr.push({
+                key: "svg" + keyIndex++,
+                title: "after " + meal,
+                width: cardWidth,
+                height: cardHeight
+            });
+        }, this);
+        return graphTilesArr;
+    };
+
+    $scope.graphTiles = creatGraphTiles();
 
     $scope.showLoader = function () {
         $scope.loading = true;
